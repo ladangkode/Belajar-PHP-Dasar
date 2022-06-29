5b19d49c37c30e931099e379a75bf2e1740ae102
efronpaduansi <eufrondpaduansi@gmail.com>
2022-06-30 03:50:30 +0700

    Add echo & print

---
+++ EchoPrint/Echo.php
@@ -0,0 +1,17 @@
+<?php
+
+    // echo & print : keduanya sama yakni untuk mencetak hasil ke dalam halaman web
+
+    // Contoh echo
+    echo "<h1>Selamat datang</h1> <br>";
+    echo "Hello world!"; echo "<br>";
+    print "Hello World"; echo "<br>";
+
+    //echo untuk mencetak sebuah variable
+    $nama = "Efron Paduansi";
+    $email = "efronius@programmer.net";
+    $umur = 23;
+
+    echo "Halo sobat, nama saya : $nama email saya : $email dan umur saya ada $umur tahun";
+
+?>
